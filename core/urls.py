292aa1334cb8878667f38123493f0d2913e792a6
ali fathi <ali.fathi.13121110@gmail.com>
2023-11-23 14:11:11 +0330

    feat:Added url:category_search/ to core/urls.py

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,7 @@ from core.forms import LoginForm
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('category_search/<str:categorys>', views.category_search, name='category search'),
     path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm), name='login'),
 ]
